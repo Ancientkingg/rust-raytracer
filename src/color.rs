0232cf1fec7f561dfb7b8b8be6ae738f1c68effa
Ancientkingg <samuelmbruin@yahoo.com>
2022-02-24 18:18:16 +0100

    added p control and readme

--- src/color.rs
+++ src/color.rs
@@ -1,14 +1,17 @@
 use nalgebra_glm as glm;
 use image;
+use rand::Rng;
 
-pub fn write_pixel(frag: glm::TVec3<f64>, _prev_color: image::Rgba<u8>, samples_per_pixel: u32, _frames: i32) -> image::Rgba<u8> {
+pub fn write_pixel(frag: glm::TVec3<f64>, _prev_color: image::Rgba<u8>, samples_per_pixel: u32, _frames: i32, render_reset_flag: bool) -> image::Rgba<u8> {
     let mut frag = frag;
     let scale = 1.0 / samples_per_pixel as f64;
     frag.x = (frag.x * scale).sqrt();
     frag.y = (frag.y * scale).sqrt();
     frag.z = (frag.z * scale).sqrt();
     let new_color = image::Rgba([(frag.x as f64 * 256.) as u8, (frag.y as f64 * 256.) as u8, (frag.z as f64 * 256.) as u8, 255]);
-    // return mix(new_color, _prev_color, _frames as f64 / (_frames as f64 + 1.0));
+    if render_reset_flag {
+        return mix(new_color, _prev_color, _frames as f64 / (_frames as f64 + 1.0));
+    }
     new_color
 }
 
@@ -23,4 +26,11 @@ fn mix(v: image::Rgba<u8>, w: image::Rgba<u8>, factor: f64) -> image::Rgba<u8> {
     let b = (v[2] as f64 * factor2 + w[2] as f64 * factor) as u8;
     let a = (v[3] as f64 * factor2 + w[3] as f64 * factor) as u8;
     image::Rgba([r,g,b,a])
+}
+
+pub fn random(min: f64, max: f64) -> glm::TVec3<f64> {
+    let r = rand::thread_rng().gen_range(min..max);
+    let g = rand::thread_rng().gen_range(min..max);
+    let b = rand::thread_rng().gen_range(min..max);
+    glm::vec3(r, g, b)
 }
--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,57 @@ const WIDTH: u32 = 1280;
 const HEIGHT: u32 = 720;
 const SAMPLES_PER_PIXEL: u32 = 1;
 const RAY_DEPTH: u8 = 20;
-const FPS: u128 = 24;
+const FPS: u128 = 10;
 const FRAME_TIME: u128 = 1000 / FPS;
+
+
+fn random_scene() -> objects::HittableList {
+    let mut world = objects::HittableList::default();
+    let ground_material = materials::Lambertian::new(glm::vec3(0.5,0.5,0.5));
+    world.push(sphere::Sphere::new(
+        glm::vec3(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_material
+    ));
+
+    for a in -2..=2 {
+        for b in -2..=2 {
+            let choose_mat = rand::thread_rng().gen::<f64>();
+            let centre: glm::TVec3<f64> = glm::vec3(
+                a as f64 + 0.9 * rand::thread_rng().gen::<f64>(),
+                0.2,
+                b as f64 + 0.9 * rand::thread_rng().gen::<f64>(),
+            );
+
+            if (centre - glm::vec3(4.0, 0.2, 0.0)).magnitude() > 0.9 {
+                if choose_mat < 0.8 {
+                    // Diffuse
+                    let albedo: glm::TVec3<f64> = color::random(0.0,1.0);
+                    let sphere_mat = materials::Lambertian::new(albedo);
+                    let sphere = sphere::Sphere::new(centre, 0.2, sphere_mat);
+    
+                    world.push(sphere);
+                } else if choose_mat < 0.95 {
+                    // Metal
+                    let albedo = color::random(0.4,1.0);
+                    let fuzz = rand::thread_rng().gen_range(0.0..0.5);
+                    let sphere_mat = materials::Metal::new(albedo, fuzz);
+                    let sphere = sphere::Sphere::new(centre, 0.2, sphere_mat);
+    
+                    world.push(sphere);
+                } else {
+                    // Glass
+                    let sphere_mat = materials::Dielectric::new(1.5);
+                    let sphere = sphere::Sphere::new(centre, 0.2, sphere_mat);
+    
+                    world.push(sphere);
+                }
+            }
+        }
+    }
+
+    world
+}
 
 fn main() {
     let mut frame_buffer: image::RgbaImage =
@@ -55,42 +104,43 @@ fn main() {
 
 
     //* WORLD
-    let mut world = objects::HittableList::default();
+    let world = random_scene();
+    // let mut world = objects::HittableList::default();
 
-    let material_ground = materials::Lambertian::new(glm::vec3(0.8, 0.8, 0.0));
-    let material_centre = materials::Lambertian::new(glm::vec3(0.1, 0.2, 0.5));
-    let material_left = materials::Dielectric::new(1.5);
-    let material_right = materials::Metal::new(glm::vec3(0.8, 0.6, 0.2), 0.0);
+    // let material_ground = materials::Lambertian::new(glm::vec3(0.8, 0.8, 0.0));
+    // let material_centre = materials::Lambertian::new(glm::vec3(0.1, 0.2, 0.5));
+    // let material_left = materials::Dielectric::new(1.5);
+    // let material_right = materials::Metal::new(glm::vec3(0.8, 0.6, 0.2), 0.0);
 
-    world.push(sphere::Sphere::new(
-        glm::vec3(0.0, -100.5, -1.0),
-        100.0,
-        material_ground,
-    ));
-    world.push(sphere::Sphere::new(
-        glm::vec3(0.0, 0.0, -1.0),
-        0.5,
-        material_centre,
-    ));
-    world.push(sphere::Sphere::new(
-        glm::vec3(-1.0, 0.0, -1.0),
-        0.5,
-        material_left.clone(),
-    ));
-    world.push(sphere::Sphere::new(
-        glm::vec3(-1.0, 0.0, -1.0),
-        -0.4,
-        material_left,
-    ));
-    world.push(sphere::Sphere::new(
-        glm::vec3(1.0, 0.0, -1.0),
-        0.5,
-        material_right,
-    ));
+    // world.push(sphere::Sphere::new(
+    //     glm::vec3(0.0, -100.5, -1.0),
+    //     100.0,
+    //     material_ground,
+    // ));
+    // world.push(sphere::Sphere::new(
+    //     glm::vec3(0.0, 0.0, -1.0),
+    //     0.5,
+    //     material_centre,
+    // ));
+    // world.push(sphere::Sphere::new(
+    //     glm::vec3(-1.0, 0.0, -1.0),
+    //     0.5,
+    //     material_left.clone(),
+    // ));
+    // world.push(sphere::Sphere::new(
+    //     glm::vec3(-1.0, 0.0, -1.0),
+    //     -0.4,
+    //     material_left,
+    // ));
+    // world.push(sphere::Sphere::new(
+    //     glm::vec3(1.0, 0.0, -1.0),
+    //     0.5,
+    //     material_right,
+    // ));
 
     //* CAMERA
-    let look_from = glm::vec3(3.0, 2.0, 2.0);
-    let look_at = glm::vec3(0.0, 0.0, -1.0);
+    let look_from = glm::vec3(13.0, 2.0, 3.0);
+    let look_at = glm::vec3(0.0, 0.0, 0.0);
     let vup = glm::vec3(0.0, 1.0, 0.0);
     let dist_to_focus = (look_from - look_at).magnitude();
     let aperture = 0.0;
@@ -114,6 +164,7 @@ fn main() {
         .unwrap();
 
     let render_reset_flag: &Cell<bool> = &Cell::new(false);
+    let mut reset_frame_count: bool = false;
 
     let mut frame_counts: Vec<i32> = vec![0; (WIDTH * HEIGHT) as usize];
     let cam: Arc<Mutex<camera::Camera>> = Arc::clone(&camera);
@@ -163,68 +214,72 @@ fn main() {
     while let Some(e) = window.next() {
         match e {
             Input(input, _) => {
-                match input {
-                    Button(button_args) => {
-                        if let piston_window::Button::Keyboard(key) = button_args.button {
-                            let camera = Arc::clone(&camera);
-                            let mut camera = camera.lock().unwrap();
-                            match button_args.state {
-                                piston_window::ButtonState::Press => {
-                                    match key {
-                                        piston_window::Key::W => {
-                                            camera.wasd[0] = true;
+                if !render_reset_flag.get() {
+                    match input {
+                        Button(button_args) => {
+                            if let piston_window::Button::Keyboard(key) = button_args.button {
+                                let camera = Arc::clone(&camera);
+                                let mut camera = camera.lock().unwrap();
+                                match button_args.state {
+                                    piston_window::ButtonState::Press => {
+                                        match key {
+                                            piston_window::Key::W => {
+                                                camera.wasd[0] = true;
+                                            }
+                                            piston_window::Key::A => {
+                                                camera.wasd[1] = true;
+                                            }
+                                            piston_window::Key::S => {
+                                                camera.wasd[2] = true;
+                                            }
+                                            piston_window::Key::D => {
+                                                camera.wasd[3] = true;
+                                            }
+                                            piston_window::Key::Space => {
+                                                camera.wasd[4] = true;
+                                            }
+                                            piston_window::Key::LCtrl => {
+                                                camera.wasd[5] = true;
+                                            }
+                                            piston_window::Key::P => {
+                                                (*render_reset_flag).set(true);
+                                            }
+                                            _ => (),
                                         }
-                                        piston_window::Key::A => {
-                                            camera.wasd[1] = true;
-                                        }
-                                        piston_window::Key::S => {
-                                            camera.wasd[2] = true;
-                                        }
-                                        piston_window::Key::D => {
-                                            camera.wasd[3] = true;
-                                        }
-                                        piston_window::Key::Space => {
-                                            camera.wasd[4] = true;
-                                        }
-                                        piston_window::Key::LCtrl => {
-                                            camera.wasd[5] = true;
-                                        }
-                                        _ => (),
                                     }
+                                    piston_window::ButtonState::Release => {
+                                        match key {
+                                            piston_window::Key::W => {
+                                                camera.wasd[0] = false;
+                                            }
+                                            piston_window::Key::A => {
+                                                camera.wasd[1] = false;
+                                            }
+                                            piston_window::Key::S => {
+                                                camera.wasd[2] = false;
+                                            }
+                                            piston_window::Key::D => {
+                                                camera.wasd[3] = false;
+                                            }
+                                            piston_window::Key::Space => {
+                                                camera.wasd[4] = false;
+                                            }
+                                            piston_window::Key::LCtrl => {
+                                                camera.wasd[5] = false;
+                                            }
+                                            _ => (),
+                                        }
+                                    },
                                 }
-                                piston_window::ButtonState::Release => {
-                                    match key {
-                                        piston_window::Key::W => {
-                                            camera.wasd[0] = false;
-                                        }
-                                        piston_window::Key::A => {
-                                            camera.wasd[1] = false;
-                                        }
-                                        piston_window::Key::S => {
-                                            camera.wasd[2] = false;
-                                        }
-                                        piston_window::Key::D => {
-                                            camera.wasd[3] = false;
-                                        }
-                                        piston_window::Key::Space => {
-                                            camera.wasd[4] = false;
-                                        }
-                                        piston_window::Key::LCtrl => {
-                                            camera.wasd[5] = false;
-                                        }
-                                        _ => (),
-                                    }
-                                },
                             }
                         }
-                    }
-                    piston_window::Input::Move(motion) => {
-                        if let piston_window::Motion::MouseRelative(pos) = motion {
-                            (*render_reset_flag).set(true);
-                            sender.send(pos).expect("Invalid Mouse Position Recorded!");
+                        piston_window::Input::Move(motion) => {
+                            if let piston_window::Motion::MouseRelative(pos) = motion {
+                                sender.send(pos).expect("Invalid Mouse Position Recorded!");
+                            }
                         }
+                        _ => ()
                     }
-                    _ => ()
                 }
             }
             Loop(l) => {
@@ -247,11 +302,18 @@ fn main() {
                                 let ray: ray::Ray = camera.lock().unwrap().get_ray(screen_coords);
                                 pixel_color += ray::ray_color(&ray, &world, RAY_DEPTH);
                             }
+                            if render_reset_flag.get() && !reset_frame_count {
+                                for i in 0..frame_counts.len() {
+                                    frame_counts[i] = 0;
+                                }
+                                reset_frame_count = true;
+                            }
                             *pixel = color::write_pixel(
                                 pixel_color,
                                 *pixel,
                                 SAMPLES_PER_PIXEL,
-                                frame_counts[(x + y * WIDTH) as usize]
+                                frame_counts[(x + y * WIDTH) as usize],
+                                render_reset_flag.get()
                             );
                             frame_counts[(x + y * WIDTH) as usize] += 1;
                         }
@@ -260,13 +322,16 @@ fn main() {
                         tex_context.encoder.flush(device);
 
                         //* FPS Counter
-                        let fps = fps_counter.tick();
-                        let fps = ((fps * 10.0).round() / 10.0).to_string();
-                        let transform = c.transform.trans(10.0, 30.0);
-                        piston_window::text::Text::new(32)
-                            .draw(&fps, &mut glyphs, &c.draw_state, transform, g)
-                            .unwrap();
-                        glyphs.factory.encoder.flush(device);
+                        if !render_reset_flag.get() {
+                            let fps = fps_counter.tick();
+                            let fps = ((fps * 10.0).round() / 10.0).to_string();
+                            let transform = c.transform.trans(10.0, 30.0);
+                            piston_window::text::Text::new(32)
+                                .draw(&fps, &mut glyphs, &c.draw_state, transform, g)
+                                .unwrap();
+                            glyphs.factory.encoder.flush(device);
+                        }
+                        
                     });
                 }
             }
